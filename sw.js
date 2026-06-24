38653645249d366c9cc82ff5e3d916a0b9e2d69d
ryan123he <ryan123he@users.noreply.github.com>
2026-06-24 14:09:30 +0800

    feat: add feedback/suggestions entry (GitHub Issues)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="jnltxkx-v1";
+﻿const C="jnltxkx-v1";
 const U=["index.html","manifest.json","icon-192.png","icon-512.png"];
 self.addEventListener("install",function(e){e.waitUntil(Promise.all([caches.open(C).then(function(c){return c.addAll(U)}),self.skipWaiting()]))});
 self.addEventListener("activate",function(e){e.waitUntil(Promise.all([caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}),self.clients.claim()]))});
