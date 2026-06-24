112d0138e6a0993ef215937f98aed0e5724ca8bf
ryan123he <ryan123he@users.noreply.github.com>
2026-06-24 13:38:43 +0800

    fix: add PNG icons, fix manifest + SW for PWA installability

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿const CACHE="jnltxkx-v1";
-const URLS=["/what-i-learned-today/","index.html","manifest.json","icon.svg"];
-self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
-self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}))});
+﻿const C="jnltxkx-v1";
+const U=["index.html","manifest.json","icon.svg"];
+self.addEventListener("install",function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(U)}))});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}))});
 self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
