e23e174c82fad13495fc538eb8b6f044df16a4c4
ryan123he <487327409@qq.com>
2026-06-24 13:50:37 +0800

    fix: add skipWaiting + clients.claim to SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿const C="jnltxkx-v1";
-const U=["index.html","manifest.json","icon.svg"];
-self.addEventListener("install",function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(U)}))});
-self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}))});
+const C="jnltxkx-v1";
+const U=["index.html","manifest.json","icon-192.png","icon-512.png"];
+self.addEventListener("install",function(e){e.waitUntil(Promise.all([caches.open(C).then(function(c){return c.addAll(U)}),self.skipWaiting()]))});
+self.addEventListener("activate",function(e){e.waitUntil(Promise.all([caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}),self.clients.claim()]))});
 self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
